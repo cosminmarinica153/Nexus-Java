d32fc584bac9fe81f62e83784f552756602b14e9
LURADU <radulung14@gmail.com>
2024-07-01 20:20:53 +0300

    blackjack.v2

---
+++ balckjack/src/Card.java
@@ -0,0 +1,28 @@
+public class Card{
+    private int value;
+    private String suits;
+    private String rank;
+
+    public Card(int value, String suits, String rank){
+        this.value = value;
+        this.suits = suits;
+        this.rank = rank;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public String getSuits() {
+        return suits;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    @Override
+    public String toString() {
+        return  rank + " of " + suits;
+    }
+}
